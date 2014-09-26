16af05010c20546d7fbde90d70faf5f4e99ef8fa
amwso <amwso@users.noreply.github.com>
2014-09-26 18:37:29 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -10,12 +10,13 @@ mysql_init () {
 	MYSQL_PASSWORD=`pwgen -c -n -1 12`
 	mkdir $MYSQL_DATA_PATH
 	chown user_mysql:user_mysql $MYSQL_DATA_PATH
-	#touch $CONF_PATH/my.cnf
-	/usr/bin/mysql_install_db --defaults-file=/dev/null --datadir=$MYSQL_DATA_PATH --user=user_mysql
-	/usr/sbin/mysqld --defaults-file=/dev/null --basedir=/usr --datadir=$MYSQL_DATA_PATH --log-error=/dev/null --user=user_mysql --pid-file=$MYSQL_DATA_PATH/mysqld.pid --socket=$MYSQL_DATA_PATH/mysqld.sock --skip-networking --plugin-dir=/usr/lib/mysql/plugin &
-	sleep 5;
-	/usr/bin/mysqladmin -S $MYSQL_DATA_PATH/mysqld.sock -u root password $MYSQL_PASSWORD
+	mysql_install_db --defaults-file=/dev/null --datadir=$MYSQL_DATA_PATH --user=user_mysql
+	mysqld --defaults-file=/dev/null --basedir=/usr --datadir=$MYSQL_DATA_PATH --log-error=/dev/null --user=user_mysql --pid-file=$MYSQL_DATA_PATH/mysqld.pid --socket=$MYSQL_DATA_PATH/mysqld.sock --skip-networking --plugin-dir=/usr/lib/mysql/plugin &
+	sleep 3;
+	mysqladmin -S $MYSQL_DATA_PATH/mysqld.sock -u root password $MYSQL_PASSWORD
+	mysql -S $MYSQL_DATA_PATH/mysqld.sock -u root -p$MYSQL_PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION; FLUSH PRIVILEGES;"
 	echo $MYSQL_PASSWORD > $LOG_FILE_PATH/mysql-root-pw.txt
+	chmod 0400 $LOG_FILE_PATH/mysql-root-pw.txt
 	killall mysqld
 }
 
@@ -25,8 +26,9 @@ check_dir () {
 	[[ ! -d $LOG_FILE_PATH/php-fpm ]] && mkdir -p $LOG_FILE_PATH/php-fpm
 	[[ ! -d $LOG_FILE_PATH/mysql ]] && mkdir -p $LOG_FILE_PATH/mysql
 	[[ ! -d $CONF_PATH ]] && mkdir -p $CONF_PATH
+
 	[[ ! -d $MYSQL_DATA_PATH ]] && mysql_init
-	[[ ! -d $WWW_DATA_PATH ]] && mkdir $WWW_DATA_PATH && chown user_web:user_web $WWW_DATA_PATH
+	[[ ! -d $WWW_DATA_PATH ]] && mkdir $WWW_DATA_PATH && chown user_app:user_app $WWW_DATA_PATH
 }
 
 check_sys_user () {
@@ -43,9 +45,15 @@ check_sys_user () {
 	done
 }
 
+update_config_file () {
+	/bin/cp /data/conf/supervisor_service.conf /etc/supervisor/conf.d/
+	/bin/cp /data/conf/my.cnf /etc/mysql/my.cnf
+	ln -s /data/mysql/mysqld.sock /var/run/mysqld/mysqld.sock
+}
+
 check_sys_user
 check_dir
-#check_config_file
+update_config_file
 
 
 
