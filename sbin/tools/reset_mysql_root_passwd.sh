b051d1cd8eb95aafe16dafc016db8a82d37d6977
amwso <amwso@users.noreply.github.com>
2015-03-10 15:19:24 +0800

    Update reset_mysql_root_passwd.sh

--- sbin/tools/reset_mysql_root_passwd.sh
+++ sbin/tools/reset_mysql_root_passwd.sh
@@ -2,13 +2,16 @@
 
 PASSWD_FILE_PATH=/data/log/mysql-root-pw.txt
 MYSQL_DATA_PATH=/data/mysql
+MYSQL_CONF_PATH=/data/conf/my.cnf
 
 MYSQL_PASSWORD=`pwgen -c -n -1 12`
 
-
-sed -i '/\[mysqld\]/ a skip-grant-tables' /data/myapp/conf/my.cnf
+sed -i '/\[mysqld\]/ a skip-grant-tables' $MYSQL_CONF_PATH
 supervisorctl restart mysqld
 mysql -S $MYSQL_DATA_PATH/mysqld.sock -u root -e "UPDATE mysql.user SET Password = PASSWORD('$MYSQL_PASSWORD') WHERE User = 'root'; GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION; FLUSH PRIVILEGES; "
 
 echo $MYSQL_PASSWORD > $PASSWD_FILE_PATH
 chmod 0400 $PASSWD_FILE_PATH
+
+sed -i '/^skip-grant-tables$/d' $MYSQL_CONF_PATH
+supervisorctl restart mysqld
