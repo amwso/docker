26668de5352cc3ca3642fcd54d0d09108ed3d24a
amwso <amwso@users.noreply.github.com>
2015-12-09 16:30:38 +0800

    update language install script

--- sbin/tools/intall_wp.sh
+++ sbin/tools/intall_wp.sh
@@ -6,7 +6,7 @@ PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 cd /tmp
 
-sudo -u user_app -- wp core download --path=/data/www --locale=zh_CN
+sudo -u user_app -- wp core download --path=/data/www
 
 sudo -u user_app -- wp core config --dbname=user_db --dbuser=user_db --dbpass=`cat /data/var/log/mysql-root-pw.txt` --dbhost=127.0.0.1 --dbprefix=wp_ --path=/data/www
 
@@ -17,5 +17,9 @@ define( 'WP_SITEURL', 'http://' . \$_SERVER['SERVER_HOST'] );
 EOF
 
 sudo -u user_app -- wp core install --url='http://' --title='hello world' --admin_user=${DOCKER_WP_ADMIN:-admin} --admin_password=${DOCKER_WP_PASSWORD:-password} --admin_email=admin@exmaple.com --path=/data/www
+
+sudo -u user_app -- wp core language install zh_CN --path=/data/www
+
+sudo -u user_app -- wp core language activate zh_CN --path=/data/www
 
 sudo -u user_app -- wp core language update --path=/data/www
